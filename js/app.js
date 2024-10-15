e8744987f808a34af75624c3a1fae2736aa9f6a6
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-15 06:59:34 +0300

    Updated validation script

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 // Start Email validation
-let form = document.getElementById('form');
-let eVEmail = document.getElementById('eVEmail').value;
-let emailVT = document.getElementById('emailVT');
-let pattern = /^[^ ] + @[^ ]+\.[a-z]{2,3}$/;
+function validate(){
+    let form = document.getElementById('form');
+    let eVEmail = document.getElementById('eVEmail').value;
+    let emailVT = document.getElementById('emailVT');
+    let pattern = /^[^ ] + @[^ ]+\.[a-z]{2,3}$/;
 
-if (eVEmail.match(pattern)){
-    form.classList.add('valid');
-    emailVT.innerHTML = 'Your Email Address s Valid.'
-}
+    if (eVEmail.match(pattern)){
+        form.classList.add('valid');
+        emailVT.innerHTML = 'Your Email Address s Valid.'
+        emailVT.style.color = green;
+    }
 
-else{
-    form.classList.remove('valid');
-    emailVT.innerHTML = 'Please Enter Valid Email'
+    else{
+        form.classList.remove('valid');
+        emailVT.innerHTML = 'Please Enter Valid Email';
+        emailVT.style.color = red;
+    }
 }
 // End of Email validation

17c8ea5ea36f77cb36b0f190d44cd6dcee748318
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-15 06:53:29 +0300

    Working on email validation script

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-e
+// Start Email validation
+let form = document.getElementById('form');
+let eVEmail = document.getElementById('eVEmail').value;
+let emailVT = document.getElementById('emailVT');
+
+
+// End of Email validation
